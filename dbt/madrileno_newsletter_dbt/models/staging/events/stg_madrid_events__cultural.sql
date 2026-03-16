bb3540e28a22c97db6554088e80f7836192c5802
lukeveitch <luke.veitch@live.co.uk>
2026-03-16 12:43:11 +0100

    finished the stg_madrid_events__cultural file. decided to keep the category column which is an array

--- dbt/madrileno_newsletter_dbt/models/staging/events/stg_madrid_events__cultural.sql
+++ dbt/madrileno_newsletter_dbt/models/staging/events/stg_madrid_events__cultural.sql
@@ -4,7 +4,30 @@ with source as (
 
 renamed as (
     select
-        article_id as id,
-        * except(article_id)
+        article_id,
+        link as article_url,
+        title,
+        description,
+        keywords,
+        creator,
+        language,
+        country,
+        category,
+        datatype,
+        safe_cast(pubDate as timestamp) as pub_date,
+        pubDateTZ as pub_date_tz,
+        safe_cast(fetched_at as timestamp) as fetched_at,
+        image_url,
+        video_url,
+        source_id,
+        source_name,
+        source_priority,
+        source_url,
+        source_icon,
+        duplicate,
+        safe_cast(extracted_at as timestamp) as extracted_at
     from source
+    where title is not null
 )
+
+select * from renamed
